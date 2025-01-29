e7e216bf6f747913c710198142dbcd3950f69b78
Alexander Dummett <alexdummett@hotmail.com>
2025-01-29 15:03:59 +0000

    feat: implementing i18n

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,15 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import "./index.css";
+import { useTranslation } from "react-i18next";
 
 const App: React.FC = () => {
+  const { t, i18n } = useTranslation();
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Test React Proj</p>
+        <p>{t("Welcome to React")}</p>
       </header>
     </div>
   );
