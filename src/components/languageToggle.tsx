2521b2e0c6dd1d5828aa614709c53b886283d01e
Alexander Dummett <alexdummett@hotmail.com>
2025-02-05 12:44:54 +0000

    feat: moving the toggle to the whole language component
    feels cleaner to have this as essentially one button with a toggle UI

--- src/components/languageToggle.tsx
+++ src/components/languageToggle.tsx
@@ -36,14 +36,13 @@ const LanguageToggle: React.FC = () => {
   };
 
   return (
-    <div className="flex items-center space-x-2">
+    <div className="flex items-center space-x-2 cursor-pointer" onClick={toggleSwitch}>
       <div>
         <Image src={noFlag} width="25px" height="20px" alt="Norsk" />
       </div>
 
       <div
-        className="w-14 h-8 flex items-center bg-gray-300 rounded-full p-1 cursor-pointer"
-        onClick={toggleSwitch}
+        className="w-14 h-8 flex items-center bg-gray-300 rounded-full p-1"
       >
         <div
           className={`bg-white w-6 h-6 rounded-full shadow-md transform duration-300 ease-in-out ${
